32b4d41e720a09b57d3a915c4375fe7f87928f61
Guillermo <guille@example.com>
2023-10-05 14:35:00 +0200

    intento añadir interfaz Operable

---
+++ src/main/java/org/iesvdm/interfaces/I_Operable.java
@@ -0,0 +1,13 @@
+package org.iesvdm.interfaces;
+
+public interface I_Operable<E> {
+
+    public E suma(E elemento);
+
+    public E resta(E elemento);
+
+    public E multiplica(E elemento);
+
+    public E divide(E elemento);
+
+}
---
+++ src/test/java/I_OperableTest.java
@@ -0,0 +1,29 @@
+import org.iesvdm.interfaces.I_Operable;
+
+public class I_OperableTest {
+
+    public class prueba<T extends Number> implements I_Operable{
+
+        // Atributos
+
+        @Override
+        public Object suma(Object elemento) {
+            return null;
+        }
+
+        @Override
+        public Object resta(Object elemento) {
+            return null;
+        }
+
+        @Override
+        public Object multiplica(Object elemento) {
+            return null;
+        }
+
+        @Override
+        public Object divide(Object elemento) {
+            return null;
+        }
+    }
+}
